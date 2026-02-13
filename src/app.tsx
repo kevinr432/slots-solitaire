8018ff50b17df41eca085c3e2b319d5862407ead
Kevin Reagan <kevin@kevinreagan.com>
2026-02-13 16:24:55 -0500

    fix icon

--- src/app.tsx
+++ src/app.tsx
@@ -724,7 +724,7 @@ export default function SlotsSolitaire() {
                           src={HELP_IMAGE_SRC}
                           style={{
                               maxWidth: "100%",
-                              maxHeight: "100vh",
+                              maxHeight: "120vh",
                               width: "auto",
                               height: "auto",
                               objectFit: "contain"
